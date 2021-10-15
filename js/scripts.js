720eca6014c247ec8f1fe77ae75ff551723be4a6
nhungle <misssg85@gmail.com>
2021-10-15 12:32:09 -0700

    update html use jquery and update styles.scss

--- js/scripts.js
+++ js/scripts.js
@@ -12,5 +12,12 @@ $(function() {
         }
         
     });
+
+    $("#reserveButton").click(function() {
+        $("#reserveModal").modal("show");
+    });
    
+    $("#loginButton").click(function() {
+        $("#loginModal").modal("show");
+    });
 });
